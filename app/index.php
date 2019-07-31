e2145ecccfc47aceaadc382dd9d7bcaeab8edeee
Ilger <ilgerdepraetere@gmail.com>
2019-07-31 13:48:05 +0200

    select data and put it in a html table.

--- app/index.php
+++ app/index.php
@@ -1,4 +1,26 @@
 <?php
+
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+} 
+
 $servername = "localhost";
 $username = "admin";
 $password = "UirxpPeM0FT7";
@@ -6,27 +28,19 @@ $dbname = "myDBPDO";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // prepare sql and bind parameters
-    $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email)
-    VALUES (:firstname, :lastname, :email)");
-    $stmt->bindParam(':firstname', $firstname);
-    $stmt->bindParam(':lastname', $lastname);
-    $stmt->bindParam(':email', $email);
-
-    // insert a row
-    $firstname = "John";
-    $lastname = "Deo";
-    $email = "oksel@example.com";
+    $stmt = $conn->prepare("SELECT id, firstname, lastname FROM MyGuests");
     $stmt->execute();
 
-    echo "New records oksel created successfully";
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
     }
-catch(PDOException $e)
-    {
+}
+catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
-    }
+}
 $conn = null;
+echo "</table>";
 ?> 
